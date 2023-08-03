85f7f85fb81293d5b05dde11423dab475aff2716
James Lamb <jlamb@ucsb.edu>
2023-08-03 11:10:40 -0600

    More greyscale conversions.

--- src/model/common.py
+++ src/model/common.py
@@ -12,12 +12,12 @@ def default_conv(in_channels, out_channels, kernel_size, bias=True):
 class MeanShift(nn.Conv2d):
     def __init__(
         self, rgb_range,
-        rgb_mean=(0.4488, 0.4371, 0.4040), rgb_std=(1.0, 1.0, 1.0), sign=-1):
+        mean=(0.5,), rgb_std=(1.0,), sign=-1):
 
-        super(MeanShift, self).__init__(3, 3, kernel_size=1)
+        super(MeanShift, self).__init__(1, 1, kernel_size=1)
         std = torch.Tensor(rgb_std)
-        self.weight.data = torch.eye(3).view(3, 3, 1, 1) / std.view(3, 1, 1, 1)
-        self.bias.data = sign * rgb_range * torch.Tensor(rgb_mean) / std
+        self.weight.data = torch.eye(1).view(1, 1, 1, 1) / std.view(1, 1, 1, 1)
+        self.bias.data = sign * rgb_range * torch.Tensor(mean) / std
         for p in self.parameters():
             p.requires_grad = False
 
@@ -63,7 +63,7 @@ class Upsampler(nn.Sequential):
         m = []
         if (scale & (scale - 1)) == 0:    # Is scale = 2^n?
             for _ in range(int(math.log(scale, 2))):
-                m.append(conv(n_feats, 4 * n_feats, 3, bias))
+                m.append(conv(n_feats, 4 * n_feats, 1, bias))
                 m.append(nn.PixelShuffle(2))
                 if bn:
                     m.append(nn.BatchNorm2d(n_feats))
@@ -73,7 +73,7 @@ class Upsampler(nn.Sequential):
                     m.append(nn.PReLU(n_feats))
 
         elif scale == 3:
-            m.append(conv(n_feats, 9 * n_feats, 3, bias))
+            m.append(conv(n_feats, 9 * n_feats, 1, bias))
             m.append(nn.PixelShuffle(3))
             if bn:
                 m.append(nn.BatchNorm2d(n_feats))
