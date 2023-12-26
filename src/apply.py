db78b9d4da0a7d65c0786cd6e44bfb72105d27fd
Anaya Khan <anayakhan@ucsb.edu>
2023-12-26 10:17:50 -0800

    apply.py progress

--- src/apply.py
+++ src/apply.py
@@ -21,7 +21,25 @@ from sklearn.model_selection import KFold
 # ask for number of epochs
 # ask for number of splits 
 
-print("Welcome to the SR program")
+print()
+print("Welcome to the SR program!")
 print("This program will allow you to run SR on your data with parameters of your choice")
+print()
 
+params = []
 
+print("Enter parameters below, D for default")
+scale = input("Scale (default - 4): ")
+batch = input("Batch Size (default - 4): ")
+epoch = input("Epochs Run (default - 15): ")
+split = input("Kfold split (default - 5): ")
+path = input("Path to your greyscale images (required): ")
+
+print("Are there any other parameters you would like to set? (Refer to the option_mod file for options)")
+rest_args = input("Format Example -> '--scale,4' : ")
+
+args = option_mod.parser.parse_args('''Enter the arguements''')
+
+def parseArgs(input):
+    print("Parsing arg: ", input)
+    
