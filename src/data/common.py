42a1128ddcc605f4a76a38c258592f532fd57d7d
Anaya Khan <anayakhan@ucsb.edu>
2024-05-15 21:59:46 -0700

    more cleaning

--- src/data/common.py
+++ src/data/common.py
@@ -5,14 +5,10 @@ import skimage.color as sc
 
 import torch
 
-# assuming low res patch size 
-# will scale upward for high res
+# grabs a patch of the data for testing based off of args
 def get_patch(*args, patch_size=96, scale=2):
-    # consider decreasing the patch size, not too much, test with different patch sizes, end result
-    ih, iw = args[0].shape[-2:]
 
-    # for testing 
-    # ih2, iw2 = args[1].shape[:2]
+    ih, iw = args[0].shape[-2:]
 
     if type(scale) is str:
         scale = int(scale)
@@ -28,41 +24,3 @@ def get_patch(*args, patch_size=96, scale=2):
     ]
 
     return ret
-   
-'''def set_channel(*args, n_channels=3):
-    def _set_channel(img):
-        if img.ndim == 2:
-            img = np.expand_dims(img, axis=2)
-
-        c = img.shape[2]
-        if n_channels == 1 and c == 3:
-            img = np.expand_dims(sc.rgb2ycbcr(img)[:, :, 0], 2)
-        elif n_channels == 3 and c == 1:
-            img = np.concatenate([img] * n_channels, 2)
-
-        return img
-
-    return [_set_channel(a) for a in args]
-
-def np2Tensor(*args, rgb_range=255):
-    def _np2Tensor(img):
-        np_transpose = np.ascontiguousarray(img.transpose((2, 0, 1))).copy()
-        tensor = torch.from_numpy(np_transpose).float()
-        tensor.mul_(rgb_range / 255)
-
-        return tensor
-
-    return [_np2Tensor(a) for a in args]'''
-
-'''def augment(*args):
-    hflip = random.random() < 0.5
-    vflip = random.random() < 0.5
-
-    def _augment(img):
-        if hflip: img = img[:, ::-1]
-        if vflip: img = img[::-1]
-        
-        return img
-
-    return [_augment(a) for a in args]'''
-
